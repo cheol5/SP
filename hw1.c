503f0b10693fc11e057c223c6005e4772f0bd6de
coh <och5405@naver.com>
2023-10-07 13:40:42 +0900

    modifying

--- hw1.c
+++ hw1.c
@@ -2,23 +2,6 @@
 #include "my.h"
 #include "hw1.h"
 
-static int	strlcpy(char *dst, char *src, size_t n)
-{
-	size_t	i;
-	size_t	len;
-
-	len = strlen(src);
-	if (!n)
-		return (len);
-	while(src[i] && i < n - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = 0;
-	return (len);
-}
-
 static int	getLength(int n)
 {
 	int	len = 0;
@@ -53,35 +36,22 @@ char *itoa(int n)
 	return(arr);
 }
 
-static void itoaHW(char *itoaArr, int num)
+void ft_memcpy(void *arr)
 {
-	int	i = 1;
 
-	itoaArr[0] = '0';
-	while (num)
-	{
-		itoaArr[i--] = num % 10 + '0';
-		num /= 10;
-	}
 }
-
-// void	initFile(int fd)
-// {
-// 	const char	*initData = "A66536";
-// 	int			size;
-
-// 	size = strlen(initData);
-
-// 	write(fd, initData, size);
-// 	lseek(fd, -(size - 1), SEEK_END);
-// 	write(fd, initData + 1, size - 1);
-// }
 
 // !! Write를 하면 자동으로 current File offset이 write다음 바이트로 변경됨.
 void Init(void)
 {
-	int		i = 0;
-	char	itoaArr[3] = {0};
+	fd = 0;
+}
+
+void InitStorage(void)
+{
+	unsigned short blockSize = MAX_STORAGE_SIZE - 1;
+	char arr[]="This is the test\n";
+	unsigned short check;
 	// WRONLY를 빼먹으면 파일 작성이 되지 않는다. 한번 만들어진 파일은 재생성 되지 않음. 
 	if ((fd = open(STORAGE_NAME, O_CREAT|O_RDWR|O_TRUNC, 0755)) < 0)
 	{
@@ -93,128 +63,131 @@ void Init(void)
 		perror("truncate error is occurd\n");
 		exit(1);
 	}
-	//block 초기화를 해야하는데 음.. 큰 블럭으로 보고 할 것인지를 결정해야해.. 
-	while (1)
-	{
-		write(fd, "A99", 3); //write
-		lseek(fd, 99 - 3 - 2, SEEK_CUR);
-		write(fd, "99", 2);
-		i += 99;
-		if (i + 99 > MAX_STORAGE_SIZE)
-		{
-			i = MAX_STORAGE_SIZE - i;
-			if (i < HEAD_TAIL_SIZE)
-				return ;
-			itoaHW(itoaArr, i);
-			write(fd, "A", 1);
-			write(fd, itoaArr, strlen(itoaArr));
-			lseek(fd, -2, SEEK_END);
-			write(fd, itoaArr, 2);
-			return ;
-		}
-	}
+	write(fd, "A", 1);
+	write(fd, &blockSize, 2);
+	lseek(fd, -2, SEEK_END);
+	write(fd, &blockSize, 2);
 }
 
-
-
-
-
-// caution : 만약 9이하의 블럭이라면  09 이렇게 저장되어야 한다. -> 해결 itoaHW
-static char	*insertDataInit(char *key, char *pBuf, int arrSize)
+static int	leftBlock(unsigned short leftBlockSize)
 {
-	char	*arr;
-	char	itoaArr[3] = {0};
-	int		keySize;
-	int		pbufSize;
-	int		itoaSize;
-
-	arr = (char *)malloc(sizeof(char) * arrSize);
-	if (!arr)
-		return (0);
-	memset(arr, 0, arrSize); // I think that initializing with 0 ensure null charcater!!
-	itoaHW(itoaArr, arrSize);
-	keySize = strlen(key);
-	pbufSize = strlen(pBuf);
-	itoaSize = strlen(itoaArr);
-	arr[0] = 'F';
-	strlcpy(&arr[1], itoaArr, itoaSize + 1);
-	strlcpy(&arr[1 + itoaSize], key, keySize + 1);
-	strlcpy(&arr[1 + itoaSize + keySize], pBuf, pbufSize + 1);
-	return (arr);
+	write(fd, "A", 1);
+	write(fd, &leftBlockSize, 2);
+	lseek(fd, leftBlockSize - BUFFER_SIZE - 2, SEEK_CUR);
+	write(fd, &leftBlockSize, 2);
 }
 
-static int	afterInsert(int leftBlockSize)
-{
-	char	*leftArr;
-	char	itoaArr[3] = {0};
-
-	leftArr = (char *)malloc(sizeof(char) * leftBlockSize + 1);
-	if (!leftArr)
-		return (0);
-	leftArr[0] = 'A';
-	itoaHW(itoaArr, leftBlockSize);
-	strlcpy(&leftArr[1], itoaArr, 3);
-	return (1);
-}
-
-//caution : fd를 전역변수로 빼던지 여기서 다시 open and close해야함. -> 해결: 우선 헤더에 선언
-//caution : insert이후의 과정이 필요하다 -> make afterInsert. 
-//logic : 동적할당 후 cpy. 이후 head정보를 보고 삽입 위치 결정.
+//동적할당 후 (mem)cpy이후 head정보를 보고 삽입 위치 결정.
 int InsertData(char* key, int keySize, char* pBuf, int bufSize)
 {
-	char	*arr;
-	char	buf[BUFFER_SIZE];
-	int		arrSize;
-	int		nbytes;
+	char			*arr;
+	unsigned short	arrSize;
+	unsigned short	blockSize;
+	char			buf[BUFFER_SIZE];
 
-	arrSize = keySize + bufSize + HEAD_TAIL_SIZE + 1; // 1:Ensure null character. 
-	arr = insertDataInit(key, pBuf, arrSize); // cpy operation.
-	lseek(fd, 0, SEEK_SET);
-	nbytes = read(fd, buf, BUFFER_SIZE - 1); // Use nbytes that notify EOF.
-	buf[BUFFER_SIZE - 1] = 0;
-	// Search Insert position.
-	while ((buf[0] == 'F' || atoi(&buf[1]) < arrSize) && nbytes)
+	arrSize = keySize + bufSize + HEAD + TAIL; 
+	arr = (char *)malloc(sizeof(char) * (arrSize));
+	if (!arr)
+		return (0);
+	memset(arr, 0, arrSize);
+	arr[0] = 'F';
+	memcpy(&arr[1], &arrSize, 2);
+	memcpy(&arr[1 + 2], &keySize, 1);
+	memcpy(&arr[1 + 2 + 1], &bufSize, 1);
+	memcpy(&arr[5], key, keySize);
+	memcpy(&arr[5 + keySize], pBuf, bufSize);
+	lseek(fd, 0, SEEK_SET); // first in fit.
+	while (read(fd, buf, BUFFER_SIZE))
 	{
-		lseek(fd, atoi(&buf[1]) - 1, SEEK_CUR);
-		nbytes = read(fd, buf, BUFFER_SIZE);
+		memcpy(blockSize, &buf[1], 2);
+		if (buf[0] == 'F' || blockSize < arrSize)
+		{
+			if (SEEK_CUR + arrSize > MAX_STORAGE_SIZE)
+			{
+				free(arr);
+				perror("There's no storage");
+				exit(1);
+			}
+			lseek(fd, blockSize - BUFFER_SIZE, SEEK_CUR);
+		}
+		else
+			break;
 	}
-	if (!nbytes)
-	{
-		if (atoi(&buf[1]) - arrSize + 1)
-		write(fd, arr, arrSize - 1);
-		//afterInsert()
-		free(arr);
-	}
+	write(fd, arr, arrSize); // offset 더해짐 주의
+	// Assumption 항상 null로 채워져있다고 가정. 삭제할 때 null처리하기!
+	if (blockSize > arrSize + HEAD + TAIL)
+		leftBlock(blockSize - arrSize);
 	else
-	{
-		perror("File is fulled\n");
-		exit(1);
-	}
-	return (arrSize - 1);
+		
+	free(arr);
+	return ;
 }
 
-// strcmp 이용
 int getDataByKey(char* key, int keySize, char* pBuf, int bufSize)
 {
-	char	buf[6];
-	int		nbytes;
-	char	*arrOfKey;
+	char			buf[HEAD];
+	char			*arrOfKey;
+	unsigned short	blockSize;
 
-	nbytes = read(fd, buf, 6);
-	// while (nbytes && )
-	// {
-	// 	keySize = atoi(buf[3]);
-	// 	arrOfKey = (char *)malloc(sizeof(char) * keySize + 1);
-	// 	arrOfKey[keySize] = 0;
-	// 	read(fd, arrOfKey, keySize)
-	// }
-	return (0);
+	while (read(fd, buf, HEAD))
+	{
+		memcpy(&blockSize, &buf[1], 2);
+		if (keySize == buf[3])
+		{
+			arrOfKey = (char *)malloc(sizeof(char) * keySize + 1);
+			arrOfKey[keySize] = 0;
+			read(fd, arrOfKey, keySize);
+			if(!strcmp(key, arrOfKey))
+			{
+				// search!!
+				free(arrOfKey);
+				// dataSize만큼 할당 후 read하고 cpy.
+				arrOfKey = (char *)malloc(sizeof(char) * buf[4]);
+				read(fd, arrOfKey, buf[4]);
+				memcpy(pBuf, arrOfKey, buf[4]);
+				free(arrOfKey);
+				return ;
+			}
+			free(arrOfKey);
+			lseek(fd, blockSize - HEAD - keySize, SEEK_CUR);
+		}
+		else
+			lseek(fd, blockSize - HEAD, SEEK_CUR);
+	}
+	return ;
 }
 
-// int RemoveDataByKey(char* key, int keySize)
-// {
+int RemoveDataByKey(char* key, int keySize)
+{
+	char			buf[HEAD];
+	char			*arrOfKey;
+	unsigned short	blockSize;
 
-// }
+	while (read(fd, buf, HEAD))
+	{
+		memcpy(&blockSize, &buf[1], 2);
+		if (keySize == buf[3])
+		{
+			arrOfKey = (char *)malloc(sizeof(char) * keySize + 1);
+			arrOfKey[keySize] = 0;
+			read(fd, arrOfKey, keySize);
+			if(!strcmp(key, arrOfKey))
+			{
+				// search!! 앞뒤블럭 탐색 후 합치기 드가자
+				free(arrOfKey);
+				arrOfKey = (char *)malloc(sizeof(char) * buf[4]);
+				read(fd, arrOfKey, buf[4]);
+				free(arrOfKey);
+				return ;
+			}
+			free(arrOfKey);
+			lseek(fd, blockSize - HEAD - keySize, SEEK_CUR);
+		}
+		else
+			lseek(fd, blockSize - HEAD, SEEK_CUR);
+	}
+	return ;
+}
 
 // void InitStorage(void)
 // {
--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 # include "hw1.h"
 
 // caution : header에서 extern 으로 선언 하고 여기서 따로 선언해서 사용해야 전역변수로 사용가능.
-int fd = 0;
-
+int	fd;
 int	main(void)
 {
-
-
+	char arr[3] = {0};
 	Init();
+	InitStorage();
 	return 0;
 }
 
